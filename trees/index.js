d0ff5a07adaf572323375fb7f3034ae7e6b82c34
Santiago Guerra <guerrasantiago19@gmail.com>
2020-03-03 16:08:38 -0500

    search method

--- trees/index.js
+++ trees/index.js
@@ -38,6 +38,48 @@ class BinarySearchTree {
     }
   }
 
+  minimum() {
+    const minimunNode = (node) => {
+      let current = node
+      while (current != null && current.left != null) {
+        current = current.left
+      }
+      return current
+    }
+
+    return minimunNode(this.root)
+  }
+
+  maximum() {
+    const maximunNode = (node) => {
+      let current = node
+      while (current != null && current.right != null) {
+        current = current.right
+      }
+      return current
+    }
+
+    return maximunNode(this.root)
+  }
+
+  search(key) {
+    const searchNode = (node, key) => {
+      if (node == null) {
+        return false
+      }
+
+      if(key < node.key ) {
+        return searchNode(node.left, key)
+      } else if(key > node.key) {
+        return searchNode(node.right, key)
+      } else {
+        return true
+      }
+
+    }
+
+    return searchNode(this.root, key)
+  }
 
   preOrderTraverse(callback) {
     const preOrderTraverseNode = (node, callback) => {
@@ -85,11 +127,31 @@ bst.insert(3)
 bst.insert(9)
 bst.insert(18)
 bst.insert(22)
+bst.insert(0)
 
 
-bst.preOrderTraverse(key => console.log(key))
-console.log('----------------------')
-bst.inOrderTraverse(key => console.log(key))
-console.log('----------------------')
-bst.postOrderTraverse(key => console.log(key))
+// bst.preOrderTraverse(key => console.log(key))
+// console.log('----------------------')
+// bst.inOrderTraverse(key => console.log(key))
+// console.log('----------------------')
+// bst.postOrderTraverse(key => console.log(key))
 
+// console.log(bst.minimum())
+// console.log(bst.maximum())
+console.log(bst.search(-1))
+
+class GenericNode {
+  constructor(key) {
+    this.firstChild = null
+    this.key = key
+    this.nextSibling = null
+  }
+}
+
+
+// let root = new GenericNode(12)
+// let children = root.firstChild = new GenericNode(6)
+// let sibling = children.nextSibling = new GenericNode(7)
+// sibling.nextSibling = new GenericNode(9)
+
+// console.log(root)
